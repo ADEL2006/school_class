785ae5ddb952f3d81016791a49a3b102401b1cf1
양원준 <anyas1398@gmail.com>
2023-06-19 23:02:45 +0900

    .

--- WEB_P/수행/2023-06-19(미완)/js/2113-1.js
+++ WEB_P/수행/2023-06-19(미완)/js/2113-1.js
@@ -1,37 +1,48 @@
-// HTML 문서 로드 완료 시 실행
-document.addEventListener("DOMContentLoaded", function() {
-    // 필요한 요소들을 참조
+document.addEventListener("DOMContentLoaded", function () {
     const addButton = document.querySelector('.btn-added');
     const addedContainer = document.querySelector('.added');
 
-    // 추가 버튼 클릭 시
-    addButton.addEventListener('click', function() {
-        // 새로운 div 요소 생성
-        const newBan = document.createElement('div');
-        newBan.className = "ban"; // 클래스 추가
+    addButton.addEventListener('click', function () {
+        if (addButton.value === "추가하기") {
+            const newBan = document.createElement('div');
+            newBan.className = "ban";
+            newBan.id = "ban-2-3";
 
-        // 학반 번호를 포함하는 span 요소 생성
-        const banBold = document.createElement('span');
-        banBold.className = "ban-bold";
-        const banBoldText = document.createTextNode('2-3');
-        banBold.appendChild(banBoldText);
+            const banBold = document.createElement('span');
+            banBold.className = "ban-bold";
+            const banBoldText = document.createTextNode('2-3');
+            banBold.appendChild(banBoldText);
 
-        // 학반 소개 텍스트를 포함하는 span 요소 생성
-        const banText = document.createElement('span');
-        const banTextContent = document.createTextNode('소프트웨어 공학과인 2학년 3반!');
-        banText.appendChild(banTextContent);
+            const banText = document.createElement('span');
+            const banTextContent = document.createTextNode('임베디드과인 2학년 3반! ');
+            banText.appendChild(banTextContent);
 
-        // 이미지 요소 생성 (예: 이미지 URL이 'image_url'인 경우)
-        const image = document.createElement('img');
-        image.src = 'image_url';
-        image.alt = '2-3 반 사진';
+            const image = document.createElement('img');
+            image.src = '../img/2-3.png';
+            image.alt = '2-3 반 사진';
+            image.className = 'ban-img';
 
-        // div 요소에 span 및 img 요소 추가
-        newBan.appendChild(banBold);
-        newBan.appendChild(banText);
-        newBan.appendChild(image);
+            const link = document.createElement('a');
+            link.href = '../img/2-3-schedule.png';
+            link.appendChild(image);
 
-        // 추가된 div를 addedContainer에 추가
-        addedContainer.appendChild(newBan);
+            const scheduleText = document.createElement('span');
+            const scheduleTextContent = document.createTextNode(' <-- 시간표를 보려면 클릭!');
+            scheduleText.appendChild(scheduleTextContent);
+
+            newBan.appendChild(banBold);
+            newBan.appendChild(banText);
+            newBan.appendChild(link);
+            newBan.appendChild(scheduleText);
+
+            addedContainer.appendChild(newBan);
+
+            addButton.value = "삭제하기";
+        } else if (addButton.value === "삭제하기") {
+            const banToRemove = document.getElementById('ban-2-3');
+            addedContainer.removeChild(banToRemove);
+
+            addButton.value = "추가하기";
+        }
     });
 });
